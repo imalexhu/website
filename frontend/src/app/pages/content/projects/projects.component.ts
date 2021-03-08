4f4d123172f34ba501260abb1590e3f0ae710ea4
iamalexhu <62089371+imalexhu@users.noreply.github.com>
2021-03-08 12:51:00 +1030

    updates

--- frontend/src/app/pages/content/projects/projects.component.ts
+++ frontend/src/app/pages/content/projects/projects.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
+  onHover(github: HTMLElement) {
+    console.log(github)
+    github.setAttribute('src', '../../../assets/GitHub-Mark-64px.png')
+  }
 
+  unHover(github: HTMLElement) {
+    github.setAttribute('src', '../../../assets/GitHub-Mark-120px-plus.png')
+  }
   ngOnInit(): void {
   }
 
